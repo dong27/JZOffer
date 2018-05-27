b968f569edc2d123b82001bdec93e830d8b1c84d
dong27 <2500102215@qq.com>
2018-05-27 20:26:10 +0800

    v1

--- src/Offer/Test17.java
+++ src/Offer/Test17.java
@@ -1,5 +1,77 @@
 package Offer;
 
 public class Test17 {
+    public static void main(String[] args){
+        //Print1ToMaxOfDigits(2);
+        Print1ToMaxOfNDigits(2);
+    }
+    public static void Print1ToMaxOfDigits(int n){
+        if(n<=0) return;
+        char[] number=new char[n];
+        for(int i=0;i<n;i++){
+            number[i]='0';
+        }
+        while (!increment(number)){
+            PrintNumber(number);
+        }
+    }
+    public static boolean increment(char[] num){
+        boolean isOverFlow=false;
+        int nTakeOver=0;
+        int n=num.length;
+        for(int i=n-1;i>=0;i--){
+            int nSum=num[i]-'0'+nTakeOver;
+            if(i==n-1)
+                nSum++;
+            if(nSum>=10){
+                if(i==0)
+                    isOverFlow=true;
+                else{
+                    nSum-=10;
+                    nTakeOver=1;
+                    num[i]=(char)('0'+nSum);
+                }
+            }
+            else{
+                num[i]=(char)('0'+nSum);
+                break;
+            }
+        }
+        return isOverFlow;
+    }
+    public static void PrintNumber(char[] num){
+        int n=num.length;
+        boolean flag=false;
+        for(int i=0;i<n;i++){
+            char ch=num[i];
+            if(ch!='0')
+                flag=true;
+            if(flag==true)
+                System.out.print(ch);
+        }
+        System.out.println();
+    }
 
+    //方法二，使用排列的方式
+    //少打印了一位数
+    public static void Print1ToMaxOfNDigits(int n){
+        if(n<=0) return;
+        char[] num=new char[n];
+        for(int i=0;i<10;i++){
+            num[0]=(char)(i+'0');
+            Print1ToMaxOfNDigitsRecursively(num,n,0);
+        }
+       // Print1ToMaxOfNDigitsRecursively(num,n,0);
+    }
+    public static void Print1ToMaxOfNDigitsRecursively(char[] num,int length,int index){
+        if(index==length)
+        {
+            PrintNumber(num);
+            return;
+        }
+        for(int i=0;i<10;i++){
+            num[index]=(char)(i+'0');
+            Print1ToMaxOfNDigitsRecursively(num,length,index+1);
+        }
+    }
 }
